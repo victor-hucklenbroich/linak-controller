1c05af2521946cd1c4ecffd2e9ef9b5ee1c92eb2
Victor Hucklenbroich <victor-h@outlook.de>
2026-06-09 21:11:49 +0200

    Add date and time stamp to logs

--- linak_controller/util.py
+++ linak_controller/util.py
@@ -3,10 +3,13 @@ Random helpers and util.
 """
 
 import asyncio
+from datetime import datetime
+
 
 class Logger:
     def log(self, message, end="\n"):
-        print(message, end=end)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{timestamp} {message}", end=end)
 
 logger = Logger()
 
